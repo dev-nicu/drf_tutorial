4c954d8ae19fba33424f86bb99850c26ecd2dc91
dev-nicu <nicolae.boboia@globallogic.com>
2024-08-14 18:55:23 +0300

    authentication and processing

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippet/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('user/<int:pk>/', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
